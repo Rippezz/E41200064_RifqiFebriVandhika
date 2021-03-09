7e74ac7413bc1b19dc95b7809d14f95183b5a22f
Rifqi Febri Vandhika <gtgrifqi@gmail.com>
2021-03-09 18:40:33 +0700

    Create Vabdat.java

---
+++ Vabdat.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.rippezz.program;
+
+/**
+ *
+ * @author RIP SNI
+ */
+public class Vabdat {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        // Belajar membuat variable
+        double FISIKA, MTK, BIOLOGI, KIMIA, TOTAL;
+        FISIKA=90;
+        MTK=94;
+        BIOLOGI=95;
+        KIMIA=96;
+        TOTAL= 4;
+        double nilai=(FISIKA+MTK+BIOLOGI+KIMIA)/4;
+        
+        // Output
+        String nama = "RIFQI FEBRI VANDHIKA";
+          
+        System.out.println("Nilai Dari: "+nama);
+        System.out.println("FISIKA= "+FISIKA);
+        System.out.println("MTK= "+MTK);
+        System.out.println("BIOLOGI= "+BIOLOGI);
+        System.out.println("Rata-Rata= "+nilai);
+        
+        
+        
+    }
+    
+}
